2ea82ef073360cf7161c35ca971bc7f315667786
Jack Boberg <jack.boberg@gmail.com>
2012-02-08 21:02:10 -0500

    added base file and updated site-packages

---
+++ jswatchr.py
@@ -0,0 +1,9 @@
+import os, time, argparse, jsmin
+from fsevents import Observer
+from ConfigParser import SafeConfigParser
+
+def main():
+  print 'all systems go...'
+
+if __name__ == "__main__":
+  main()
