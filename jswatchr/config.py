8c564cc40cc2fc32a4c708d579aa22bea039ba24
Jack Boberg <jack.boberg@gmail.com>
2012-02-20 18:29:17 -0500

    implement recusrive parsing and inline script support move global config to a independent module

---
+++ jswatchr/config.py
@@ -0,0 +1,8 @@
+# what file extensions should we monitor
+source_ext = 'xjs'
+exts = ['js', source_ext]
+# TODO: support ignoring pre-minified and output files
+#ignore_exts = ['min.js']
+sources = []
+source_dir = None
+dest_dir = None
--- jswatchr/parse.py
+++ jswatchr/parse.py
@@ -1,26 +1,70 @@
 import jsmin
 import os
-# perform action on matched file
-def parse_file(groups, src, dest):
+import config
+
+cmds = ['@import'] # TODO: support other commands '@require'
+
+def parse_file(src):
     """
-    parse arguments and make go
+    find file in config and output to dest dir
     """
-    for (group,scripts) in groups:
-        if src in scripts:
-            #split extension off of file
-            (root,ext) = os.path.splitext(group)
-            #split root into path,group name
-            (source_path,group_name) = os.path.split(root)
-            #if destination directory was defined in CLI use it, otherwise place next to xjs file
-            if dest == None:
+    for (master,scripts) in config.sources:
+        if src == master or src in scripts:
+            # split extension off of file
+            (root,ext) = os.path.splitext(master)
+            # split root into path, base file name
+            (source_path,base_file) = os.path.split(root)
+            # if destination directory was defined in CLI use it, otherwise place next to xjs file
+            if config.dest_dir == None:
                 dest = source_path
-            min_scripts = '';
-            for script in scripts:
-                #open file and minify
-                js = open(script).read()
-                min_scripts += js
-            output_file = dest + '/' + group_name + '.min.js'
+            else:
+                dest = config.dest_dir
+            output = get_output(master)
+            output_file = dest + '/' + base_file + '.min.js'
             f = open(output_file,'w')
-            f.write(jsmin.jsmin(min_scripts))
+            f.write(jsmin.jsmin(output))
             f.close()
             print "Wrote combined and minified file to: %s" % (output_file)
+
+def get_output(src):
+    """
+    parse lines looking for commands
+    """
+    output = ''
+    lines = open(src, 'rU').readlines()
+    for line in lines:
+        segs = line.split()
+        if len(segs) > 0 and segs[0] in cmds:
+            # determine file path, recurse
+            (source_path,base_file) = os.path.split(src)
+            # TODO: handle user adding a new import line to dependency
+            # file, need to rebuild config.sources for this 'master'
+            output += get_output(os.path.abspath(source_path + '/' + segs[1]))
+        else:
+            output += line
+    return output
+
+def execute(cmd, line):
+    """
+    get output from command
+    """
+    return
+
+def compile(output, src, dest):
+    """
+    write minified compiled output to dest
+    """
+    return
+
+def dependencies(src):
+    scripts = []
+    lines = open(src).readlines()
+    for line in lines:
+        segs = line.split()
+        if len(segs) > 0 and segs[0] in cmds:
+            # determine file path, recurse
+            (source_path,base_file) = os.path.split(src)
+            dep_src = os.path.abspath(source_path + '/' + segs[1])
+            scripts.append(dep_src)
+            scripts.extend(dependencies(dep_src))
+    return scripts
