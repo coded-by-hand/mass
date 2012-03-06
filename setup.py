d2eebe829b674e1e5d7554ecd9f0ef31007b8b46
Alex Padgett <alex.p@fuse.net>
2012-03-06 11:51:37 -0500

    Updated name to mass Update documentation

--- setup.py
+++ setup.py
@@ -3,18 +3,18 @@
 from setuptools import setup
 
 setup(
-    name='jswatchr',
+    name='mass',
     version='0.0.1',
     description='Watches your javascript',
     author='Jack Boberg & Alex Padgett',
     author_email='developer@topicdesign.com',
     url='https://github.com/jackboberg/jswatchr',
-    packages=['jswatchr'],
+    packages=['mass'],
     install_requires=['distribute','jsmin','macfsevents'],
     zip_safe = False,
     entry_points = {
         'console_scripts': [
-            "jswatchr = jswatchr.monitor:main"
+            "mass = jswatchr.monitor:main"
         ],
     }
 )
